8bc5f1b62668f5c4166cd61ef0a2bc02c100f5ef
Johan Swan <jswannieman@gmail.com>
2023-09-25 18:03:12 +1300

    Update usersSlice Javascript Component

--- src/features/users/usersSlice.js
+++ src/features/users/usersSlice.js
@@ -3,11 +3,13 @@ import { createSlice } from "@reduxjs/toolkit";
 const initialState = [
   { id: "0", name: "Luke Green" },
   { id: "1", name: "Thomas Sheehan" },
-  { id: "2", name: "Paul Frost" },
-  { id: "3", name: "Graeme Proops" },
-  { id: "4", name: "Dennis Dateling" },
-  { id: "5", name: "Mark Willcox" },
-  { id: "6", name: "Danny Redpath" },
+  { id: "2", name: "Billy Brown" },
+  { id: "3", name: "Peter Pan" },
+  { id: "4", name: "Paul Frost" },
+  { id: "5", name: "Graeme Proops" },
+  { id: "6", name: "Dennis Dateling" },
+  { id: "7", name: "Mark Willcox" },
+  { id: "8", name: "Danny Redpath" },
 ];
 
 const usersSlice = createSlice({
